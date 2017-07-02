22401b6803e5f5669c00708eec757ec91382fa8a
Amit Metkar <amit.metkat@gmail.com>
2017-07-02 21:32:04 +0530

    Property & event binding

--- my-first-angular2-app/src/app/servers/servers.component.ts
+++ my-first-angular2-app/src/app/servers/servers.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  //selector: 'app-servers',
+  selector: 'app-servers',
   //selector: '[app-servers]',
-  selector: '.app-servers',
-  //templateUrl: './servers.component.html',
-  template: `
-      <app-server></app-server>
-      <app-server></app-server>`,
+  //selector: '.app-servers',
+  templateUrl: './servers.component.html',
+  // template: `
+  //     <app-server></app-server>
+  //     <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server is created!';
+  
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created!';
+  }
 }
